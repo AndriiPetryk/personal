2bb618737a97463327dc4aa598a99bf07b3f90a8
Petryk Andrii EXT WABCO_EXT <andrii.petryk.external@zf.com>
2022-05-06 14:19:45 +0300

    added build

---
+++ build/precache-manifest.da1adeef86718d392caddc5eb8fc156e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119affdd11bcc2407a85e6f1e3d73fa9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5bf8cc171ff7ef528be7",
+    "url": "/static/css/main.b1204bcd.chunk.css"
+  },
+  {
+    "revision": "c2c549af8e68b484f5eb",
+    "url": "/static/js/2.26f798e8.chunk.js"
+  },
+  {
+    "revision": "bfbf98b39da508f522b8cab91a8e0702",
+    "url": "/static/js/2.26f798e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d93e759cb480ead1810a",
+    "url": "/static/js/3.a1d4c0d5.chunk.js"
+  },
+  {
+    "revision": "5bf8cc171ff7ef528be7",
+    "url": "/static/js/main.a59af803.chunk.js"
+  },
+  {
+    "revision": "496f18f43d07b327353c",
+    "url": "/static/js/runtime-main.0d40dbd2.js"
+  },
+  {
+    "revision": "013cbb90e747fe382f9297c8981dd857",
+    "url": "/static/media/avatar.013cbb90.jpg"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/static/media/coffee-apple.2dd213aa.jpg"
+  }
+]);
